658976b5c4ce8995dd8d689c45d51a971a8106cd
Michael Dzikowski <micdz@microsoft.com>
2020-07-28 12:15:40 -0400

    updated script to add output report and region tag

--- Resize-Vm.ps1
+++ Resize-Vm.ps1
@@ -12,66 +12,30 @@
     Specifies the Sku size for the virtual machine
 .Parameter AvailabilitySetName
     Specifies the availabiltiy set name
+.Parameter ReportPath
+    Specifies the path of report output
 .NOTES
     CSV fields: Hostname, ResourceGroup, ToBeVmSize, AvailabilitySet (optional)
 
 .EXAMPLE
-    . .\Resize-Vm.ps1; Resize-Vm -AvailabilitySetName "AS1"  -ResourceGroup RG1 -NewVmSize Standard_DS3_v2
+    . .\Resize-Vm.ps1; Resize-Vm -AvailabilitySetName "AS1"  -ResourceGroup RG1 -NewVmSize Standard_DS3_v2 -ReportPath C:\temp
 
-    . .\Resize-Vm.ps1; Resize-Vm -VmList VM1, VM2 -ResourceGroup RG1 -NewVmSize Standard_DS2_v2
+    . .\Resize-Vm.ps1; Resize-Vm -VmList VM1, VM2 -ResourceGroup RG1 -NewVmSize Standard_DS2_v2 -ReportPath C:\temp
 
-    . .\Resize-Vm -PathToCsv C:\Path.csv
+    . .\Resize-Vm -PathToCsv C:\Path.csv -ReportPath C:\temp
 #>
 [CmdletBinding()]
 param
 (
     [Parameter(mandatory=$false)]
     [string]
-    $PathtoCsv
+    $PathtoCsv,
+
+    [Parameter(mandatory=$true)]
+    [string]
+    $ReportPath
 )
-if($PathtoCsv)
-{
-    $ResizeExtract = Import-Csv -Path $PathtoCsv
 
-    $availabilitySets = $ResizeExtract | Select-Object -Property AvailabilitySet, ResourceGroup, ToBeVmSize -Unique
-
-    foreach($hostname in $ResizeExtract)
-    {
-        if(!$hostname.AvailabilitySet)
-        {
-            Resize-Vm -VmList $hostname.hostname -NewVmSize $hostname.ToBeVmSize -ResourceGroup $hostname.ResourceGroup
-        }
-    }
-    foreach ($as in $availabilitySets)
-    {
-        If($as.AvailabilitySet -ne "" -or $null)
-        {
-            $availabilitySet = Get-AzAvailabilitySet -Name $as.AvailabilitySet
-            $vmIds = $availabilitySet.VirtualMachinesReferences
-                foreach ($vmid in $vmIds)
-                {
-                    $string = $vmID.Id.Split("/")
-                    $vmName = $string[8]
-
-                if($ResizeExtract.hostname -icontains $vmName)
-                {
-                    $continue =  $true
-                }
-                else
-                {
-                    $continue = $false
-                    Write-Host "Virtual Machine" $Vmname "found in Availability Set" $as.AvailabilitySet "that is not listed in extract file. Stopping script from continuing."
-                    exit
-                }
-            }
-            if($continue -eq $true)
-            {
-                Write-Host "Verified virtual machines in Availability Set are listed in extract file - continuing with resize of VMS"
-                Resize-Vm -AvailabilitySetName $as.AvailabilitySet  -ResourceGroup $as.ResourceGroup -NewVmSize $as.ToBeVmSize
-            }
-        }
-    }
-}
 Function Resize-Vm {
 
 [CmdletBinding()]
@@ -97,15 +61,26 @@ param
     $AvailabilitySetName
 )
 
-if(!$availabilitySetName -or $availabilitySetName -eq "")
+# CSV FileName
+$CSVFileName = 'VmResizeReport ' + $(Get-Date -f yyyy-MM-dd) + '.csv'
+
+# Creating DataTable Structure
+Write-Verbose 'Creating DataTable Structure'
+$DataTable = New-Object System.Data.DataTable
+$DataTable.Columns.Add("Hostname","string") | Out-Null
+$DataTable.Columns.Add("ResourceGroup","string") | Out-Null
+$DataTable.Columns.Add("NewVMSize","string") | Out-Null
+
+#region resize virtual machine not in an availability set
+if(!$availabilitySetName -and $VmList)
 {
     foreach($virtualMachine in $vmList)
     {
         $vmSize = Get-AzVMSize -ResourceGroupName $resourceGroup -VMName $virtualMachine
         $vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $virtualMachine
 
-    if($vmSize.name -contains $newVmSize)
-    {
+        if($vmSize.name -contains $newVmSize)
+        {
         Write-Host '----------------------------------------------------------------------------------------'
         Write-Host 'Sku for resize found on hardware cluster' $($vm.AvailabilitySetReference.id)
         Write-Host '----------------------------------------------------------------------------------------'
@@ -129,6 +104,16 @@ if(!$availabilitySetName -or $availabilitySetName -eq "")
                 Write-Host '----------------------------------------------------------------------------------------'
                 Write-Host 'Resize of Virtual Machine' $vm.Name "is" $job.State
                 Write-Host '----------------------------------------------------------------------------------------'
+
+                # Check VM Size after updating
+                $vmSize = Get-AzVMSize -ResourceGroupName $resourceGroup -VMName $virtualMachine
+
+                # Append data to CSV File
+                $NewRow = $DataTable.NewRow()
+                $NewRow.Hostname = $($vm.Name)
+                $NewRow.ResourceGroup = $($vm.ResourceGroupName)
+                $NewRow.NewVmSize = $($vm.HardwareProfile.VmSize)
+                $DataTable.Rows.Add($NewRow)
             }
             else
             {
@@ -139,6 +124,9 @@ if(!$availabilitySetName -or $availabilitySetName -eq "")
         }
     }
 }
+#endregion
+
+#region Resize virtual machines in an availability set
 else
 {
     $availabilitySetShutdown = Get-AzAvailabilitySet -Name $availabilitySetName
@@ -190,6 +178,13 @@ else
                     Write-Host "Starting VM:" $vmName
                     Start-Sleep 5
                 } while ($status.State -eq "Running")
+
+                # Append data to CSV File
+                $NewRow = $DataTable.NewRow()
+                $NewRow.Hostname = $($vm.Name)
+                $NewRow.ResourceGroup = $($vm.ResourceGroupName)
+                $NewRow.NewVmSize = $($vm.HardwareProfile.VmSize)
+                $DataTable.Rows.Add($NewRow)
             }
             else
             {
@@ -207,4 +202,60 @@ else
             }
         }
     }
+    $DataTable | Export-Csv "$ReportPath\$CSVFileName" -NoTypeInformation -Append -Force
+}
+#endregion
+
+if($PathtoCsv)
+{
+    
+    # Import extract file data
+    $ResizeExtract = Import-Csv -Path $PathtoCsv
+
+    # Find availability sets in csv
+    $availabilitySets = $ResizeExtract | Select-Object -Property AvailabilitySet, ResourceGroup, ToBeVmSize -Unique
+
+    #region Resize virtual machines not in an availability set
+    foreach($hostname in $ResizeExtract)
+    {
+        if(!$hostname.AvailabilitySet)
+        {
+            Resize-Vm -VmList $hostname.hostname -NewVmSize $hostname.ToBeVmSize -ResourceGroup $hostname.ResourceGroup
+        }
+    }
+    #endregion
+
+    #region Resize all machines in an availability set
+    foreach ($as in $availabilitySets)
+    {
+        If($as.AvailabilitySet -ne "" -or $null)
+        {
+            $availabilitySet = Get-AzAvailabilitySet -Name $as.AvailabilitySet
+            $vmIds = $availabilitySet.VirtualMachinesReferences
+                foreach ($vmid in $vmIds)
+                {
+                    $string = $vmID.Id.Split("/")
+                    $vmName = $string[8]
+                # Check that all virtual machines listed in csv file are in availability
+                if($ResizeExtract.hostname -icontains $vmName)
+                {
+                    $continue =  $true
+                }
+                # If machine is found that is not inside csv file stop script
+                else
+                {
+                    $continue = $false
+                    Write-Host "Virtual Machine" $Vmname "found in Availability Set" $as.AvailabilitySet "that is not listed in extract file. Stopping script from continuing."
+                    exit
+                }
+            }
+            # Continue if all virtual machines are inside csv file
+            if($continue -eq $true)
+            {
+                Write-Host "Verified virtual machines in Availability Set are listed in extract file - continuing with resize of VMS"
+                Resize-Vm -AvailabilitySetName $as.AvailabilitySet  -ResourceGroup $as.ResourceGroup -NewVmSize $as.ToBeVmSize
+            }
+        }
+    }
+    #endregion
 }
