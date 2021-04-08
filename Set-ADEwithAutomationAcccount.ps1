d587891105ef685628fdcdcfe53241fdddd61ee9
Mike Dzikowski <you@example.com>
2021-04-08 12:02:02 -0400

    initial commit

---
+++ Set-ADEwithAutomationAcccount.ps1
@@ -0,0 +1,101 @@
+# script based on work found here: https://github.com/Azure/Azure-Security-Center/blob/master/Remediation%20scripts/Disk%20encryption%20should%20be%20applied%20on%20virtual%20machines/Powershell/Enable-AzureVMDiskEncryption.ps1
+
+Import-Module Azure
+Import-Module Az.Accounts
+Import-Module Az.Security
+Import-Module Az.Compute
+Import-Module Az.KeyVault
+
+# Get the connection "AzureRunAsConnection"
+$connectionName = "AzureRunAsConnection"
+
+# Connect to Azure (Azure US Government) 
+$servicePrincipalConnection = Get-AutomationConnection -Name $connectionName   
+
+try 
+{
+        Connect-AzAccount `
+        -EnvironmentName AzureCloud `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint | Out-Null
+}
+catch
+{
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $ConnectionName not found."
+        throw $ErrorMessage
+    }
+    else
+    {
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+# Find Subscription 
+$subscriptions = Get-AzSubscription -SubscriptionName "demosub"
+
+# Loop Through Subs
+foreach($Subscription in $Subscriptions)
+{
+    $Id = ($Subscription.Id)
+    Select-AzSubscription $Id
+    # Get Security Task for Storage Security
+    $SecurityTasks += Get-AzSecurityTask | Where-Object {$_.RecommendationType -eq "EncryptionOnVm"} 
+    write-host "$securityTasks"
+}
+write-host "$securityTasks"
+
+foreach($SecurityTask in $SecurityTasks)
+{
+    $sub = $securityTask.Id.Split("/")[2]
+    $vm = $securityTask.ResourceId.Split("/")[8]
+    $vmlocation = (Get-AzVm -Name $vm).Location
+    $vmrg = (Get-AzVm -Name $vm).ResourceGroupName
+    Select-AzSubscription $sub
+
+    # Check for Existing Keyvault
+    [array]$vaultnames = "ADEVaultDEMO"
+    [array]$localvault = $null
+    $vaultnames += (Get-AzKeyVault).VaultName
+    foreach ($vaultname in $vaultnames)
+    {
+        $vaultdetails = Get-AzKeyVault -VaultName $vaultname
+        
+        # Find local KVs with Disk Encryption Flag
+        if(($vaultdetails.EnabledForDiskEncryption -eq $true) -and ($vmlocation -eq $vaultdetails.Location))
+        {
+        $localvault += $vaultdetails.VaultName
+        }
+    }
+        
+    # Use local KV if one exists
+    if ($localvault -ne $null)
+    {
+    $localvaultdetails = Get-AzKeyVault -VaultName $localvault[0]
+
+    write-host $localvaultdetails
+    
+    # Encrypt VM using existing KV 
+    Set-AzVMDiskEncryptionExtension -ResourceGroupName $vmrg -VMName $vm -DiskEncryptionKeyVaultUrl $localvaultdetails.VaultUri -DiskEncryptionKeyVaultId $localvaultdetails.ResourceId -VolumeType All -SkipVmBackup -Force
+    } 
+
+    # If no local KV exists, create one.
+    else 
+    {
+    # Create KV with unique name, allowing for long location names
+    $subid = $sub.split("-")[0].substring(0,4)
+    $vaultname = "${vmlocation}${subid}"
+    $vaultRG = "DiskEncryptionRG-${vmlocation}"
+    New-AzResourceGroup –Name $vaultRG –Location $vmlocation
+    New-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG -Location $vmlocation -EnableSoftDelete -EnabledForDeployment -EnabledForTemplateDeployment -EnabledForDiskEncryption
+    $kvid = (Get-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG).ResourceId
+    $kvurl = (Get-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG).VaultUri
+
+    # Encrypt VM
+    $encryptStatus = Set-AzVMDiskEncryptionExtension -ResourceGroupName $vmrg -VMName $vm -DiskEncryptionKeyVaultUrl $kvurl -DiskEncryptionKeyVaultId $kvid -VolumeType All -SkipVmBackup -Force 
+    Write-Output $encryptStatus
+    }
+}
