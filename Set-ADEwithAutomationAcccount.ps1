f65058568cd061df2ff18ba5aa3b18a313427647
Mike Dzikowski <34066455+mikedzikowski@users.noreply.github.com>
2021-04-09 13:45:19 -0400

    Update Set-ADEwithAutomationAcccount.ps1
    synopsis

--- Set-ADEwithAutomationAcccount.ps1
+++ Set-ADEwithAutomationAcccount.ps1
@@ -1,5 +1,18 @@
 # script based on work found here: https://github.com/Azure/Azure-Security-Center/blob/master/Remediation%20scripts/Disk%20encryption%20should%20be%20applied%20on%20virtual%20machines/Powershell/Enable-AzureVMDiskEncryption.ps1
 
+<#.SYNOPSIS
+        Simple Script that can be used with an Azure Automation Account to upate ADE settings.
+.DESCRIPTION
+         Simple Script that can be used with an Azure Automation Account to upate ADE settings.
+.Parameter Keyvault
+		Specifies the Vault name where key will be set.
+.Parameter CloudEnvironment
+		Specifies the cloud (i.e. AzureCloud vs AzureUSGovernment).  
+.NOTES
+    script based on work found here: https://github.com/Azure/Azure-Security-Center/blob/master/Remediation%20scripts/Disk%20encryption%20should%20be%20applied%20on%20virtual%20machines/Powershell/Enable-AzureVMDiskEncryption.ps1
+#>
+
+
 Import-Module Azure
 Import-Module Az.Accounts
 Import-Module Az.Security
@@ -86,28 +99,28 @@ foreach($SecurityTask in $SecurityTasks)
     # Use local KV if one exists
     if ($localvault -ne $null)
     {
-    $localvaultdetails = Get-AzKeyVault -VaultName $localvault[0]
+        $localvaultdetails = Get-AzKeyVault -VaultName $localvault[0]
 
-    write-host $localvaultdetails
-    
-    # Encrypt VM using existing KV 
-    Set-AzVMDiskEncryptionExtension -ResourceGroupName $vmrg -VMName $vm -DiskEncryptionKeyVaultUrl $localvaultdetails.VaultUri -DiskEncryptionKeyVaultId $localvaultdetails.ResourceId -VolumeType All -SkipVmBackup -Force
+        write-host $localvaultdetails
+
+        # Encrypt VM using existing KV 
+        Set-AzVMDiskEncryptionExtension -ResourceGroupName $vmrg -VMName $vm -DiskEncryptionKeyVaultUrl $localvaultdetails.VaultUri -DiskEncryptionKeyVaultId $localvaultdetails.ResourceId -VolumeType All -SkipVmBackup -Force
     } 
 
     # If no local KV exists, create one.
     else 
     {
-    # Create KV with unique name, allowing for long location names
-    $subid = $sub.split("-")[0].substring(0,4)
-    $vaultname = "${vmlocation}${subid}"
-    $vaultRG = "DiskEncryptionRG-${vmlocation}"
-    New-AzResourceGroup –Name $vaultRG –Location $vmlocation
-    New-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG -Location $vmlocation -EnableSoftDelete -EnabledForDeployment -EnabledForTemplateDeployment -EnabledForDiskEncryption
-    $kvid = (Get-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG).ResourceId
-    $kvurl = (Get-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG).VaultUri
+        # Create KV with unique name, allowing for long location names
+        $subid = $sub.split("-")[0].substring(0,4)
+        $vaultname = "${vmlocation}${subid}"
+        $vaultRG = "DiskEncryptionRG-${vmlocation}"
+        New-AzResourceGroup –Name $vaultRG –Location $vmlocation
+        New-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG -Location $vmlocation -EnableSoftDelete -EnabledForDeployment -EnabledForTemplateDeployment -EnabledForDiskEncryption
+        $kvid = (Get-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG).ResourceId
+        $kvurl = (Get-AzKeyVault -VaultName $vaultname -ResourceGroupName $vaultRG).VaultUri
 
-    # Encrypt VM
-    $encryptStatus = Set-AzVMDiskEncryptionExtension -ResourceGroupName $vmrg -VMName $vm -DiskEncryptionKeyVaultUrl $kvurl -DiskEncryptionKeyVaultId $kvid -VolumeType All -SkipVmBackup -Force 
-    Write-Output $encryptStatus
+        # Encrypt VM
+        $encryptStatus = Set-AzVMDiskEncryptionExtension -ResourceGroupName $vmrg -VMName $vm -DiskEncryptionKeyVaultUrl $kvurl -DiskEncryptionKeyVaultId $kvid -VolumeType All -SkipVmBackup -Force 
+        Write-Output $encryptStatus
     }
 }
